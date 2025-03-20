2c873986f65c28e80311140e97e512ead2755664
Armandoyael06 <armandojael06@gmail.com>
2025-03-19 18:59:26 -0600

    configuracion de botones de inicio

--- HotelBook/Form1.cs
+++ HotelBook/Form1.cs
@@ -16,5 +16,20 @@ namespace HotelBook
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
