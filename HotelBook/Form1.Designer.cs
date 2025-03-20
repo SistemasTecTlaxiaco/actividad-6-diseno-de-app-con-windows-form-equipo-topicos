208a884b3e36eb781db59c4b11cb7c19fcd1c7aa
Armandoyael06 <armandojael06@gmail.com>
2025-03-20 14:32:16 -0600

    dzfbgztgbz

--- HotelBook/Form1.Designer.cs
+++ HotelBook/Form1.Designer.cs
@@ -203,6 +203,7 @@
             this.Controls.Add(this.btnRegistrar);
             this.Name = "Form1";
             this.Text = ".";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
