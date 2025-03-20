f5d318bd2df4bc871200b6f15850384ae8defb94
Jazlynn22 <jazzlinn.bv@gmail.com>
2025-03-19 19:59:39 -0600

    Nombre de los botones

--- Reportes/Form1.cs
+++ Reportes/Form1.cs
@@ -16,5 +16,15 @@ namespace Reportes
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
