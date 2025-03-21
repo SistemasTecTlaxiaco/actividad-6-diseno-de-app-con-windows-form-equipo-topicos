354330961d5d5fb2b12ab03a2319a351e2b92829
Armandoyael06 <armandojael06@gmail.com>
2025-03-20 21:22:47 -0600

    configuracion de botones para el sistema de hoteles

--- HotelBook/Form1.Designer.cs
+++ HotelBook/Form1.Designer.cs
@@ -28,44 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRegistrar = new System.Windows.Forms.Button();
-            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.btnregistrar = new System.Windows.Forms.Button();
+            this.btnsesion = new System.Windows.Forms.Button();
             this.btnCerarsesion = new System.Windows.Forms.Button();
             this.btnModificraperfil = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.idusuario = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
+            this.nombreusuario = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.apellidousuario = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.emailusuario = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
+            this.contraseñausuario = new System.Windows.Forms.Label();
             this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.clienteaodministrador = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // btnRegistrar
+            // btnregistrar
             // 
-            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrar.Location = new System.Drawing.Point(598, 40);
-            this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(163, 50);
-            this.btnRegistrar.TabIndex = 0;
-            this.btnRegistrar.Text = "Registrar";
-            this.btnRegistrar.UseVisualStyleBackColor = true;
-            this.btnRegistrar.Click += new System.EventHandler(this.button1_Click);
+            this.btnregistrar.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnregistrar.Location = new System.Drawing.Point(598, 40);
+            this.btnregistrar.Name = "btnregistrar";
+            this.btnregistrar.Size = new System.Drawing.Size(163, 50);
+            this.btnregistrar.TabIndex = 0;
+            this.btnregistrar.Text = "Registrar";
+            this.btnregistrar.UseVisualStyleBackColor = true;
+            this.btnregistrar.Click += new System.EventHandler(this.button1_Click);
             // 
-            // btnIniciarSesion
+            // btnsesion
             // 
-            this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciarSesion.Location = new System.Drawing.Point(598, 103);
-            this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(163, 51);
-            this.btnIniciarSesion.TabIndex = 1;
-            this.btnIniciarSesion.Text = "iniciar sesion ";
-            this.btnIniciarSesion.UseVisualStyleBackColor = true;
-            this.btnIniciarSesion.Click += new System.EventHandler(this.button2_Click);
+            this.btnsesion.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsesion.Location = new System.Drawing.Point(598, 103);
+            this.btnsesion.Name = "btnsesion";
+            this.btnsesion.Size = new System.Drawing.Size(163, 51);
+            this.btnsesion.TabIndex = 1;
+            this.btnsesion.Text = "iniciar sesion ";
+            this.btnsesion.UseVisualStyleBackColor = true;
+            this.btnsesion.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnCerarsesion
             // 
@@ -76,6 +76,7 @@
             this.btnCerarsesion.TabIndex = 2;
             this.btnCerarsesion.Text = "cerrar sesion";
             this.btnCerarsesion.UseVisualStyleBackColor = true;
+            this.btnCerarsesion.Click += new System.EventHandler(this.btnCerarsesion_Click);
             // 
             // btnModificraperfil
             // 
@@ -86,16 +87,18 @@
             this.btnModificraperfil.TabIndex = 3;
             this.btnModificraperfil.Text = "Modificar perfil";
             this.btnModificraperfil.UseVisualStyleBackColor = true;
+            this.btnModificraperfil.Click += new System.EventHandler(this.btnModificraperfil_Click);
             // 
-            // label1
+            // idusuario
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(26, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 25);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "ID de usuario:";
+            this.idusuario.AutoSize = true;
+            this.idusuario.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.idusuario.Location = new System.Drawing.Point(26, 49);
+            this.idusuario.Name = "idusuario";
+            this.idusuario.Size = new System.Drawing.Size(122, 25);
+            this.idusuario.TabIndex = 4;
+            this.idusuario.Text = "ID de usuario:";
+            this.idusuario.Click += new System.EventHandler(this.idusuario_Click);
             // 
             // textBox1
             // 
@@ -105,15 +108,16 @@
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // label2
+            // nombreusuario
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 25);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Nombre:";
+            this.nombreusuario.AutoSize = true;
+            this.nombreusuario.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombreusuario.Location = new System.Drawing.Point(28, 92);
+            this.nombreusuario.Name = "nombreusuario";
+            this.nombreusuario.Size = new System.Drawing.Size(79, 25);
+            this.nombreusuario.TabIndex = 6;
+            this.nombreusuario.Text = "Nombre:";
+            this.nombreusuario.Click += new System.EventHandler(this.nombreusuario_Click);
             // 
             // textBox2
             // 
@@ -122,15 +126,15 @@
             this.textBox2.Size = new System.Drawing.Size(363, 20);
             this.textBox2.TabIndex = 7;
             // 
-            // label3
+            // apellidousuario
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(28, 129);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(80, 25);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Apellido:";
+            this.apellidousuario.AutoSize = true;
+            this.apellidousuario.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.apellidousuario.Location = new System.Drawing.Point(28, 129);
+            this.apellidousuario.Name = "apellidousuario";
+            this.apellidousuario.Size = new System.Drawing.Size(80, 25);
+            this.apellidousuario.TabIndex = 8;
+            this.apellidousuario.Text = "Apellido:";
             // 
             // textBox3
             // 
@@ -139,15 +143,15 @@
             this.textBox3.Size = new System.Drawing.Size(363, 20);
             this.textBox3.TabIndex = 9;
             // 
-            // label4
+            // emailusuario
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(28, 166);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 25);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Email @:";
+            this.emailusuario.AutoSize = true;
+            this.emailusuario.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailusuario.Location = new System.Drawing.Point(28, 166);
+            this.emailusuario.Name = "emailusuario";
+            this.emailusuario.Size = new System.Drawing.Size(82, 25);
+            this.emailusuario.TabIndex = 10;
+            this.emailusuario.Text = "Email @:";
             // 
             // textBox4
             // 
@@ -156,15 +160,15 @@
             this.textBox4.Size = new System.Drawing.Size(363, 20);
             this.textBox4.TabIndex = 11;
             // 
-            // label5
+            // contraseñausuario
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(26, 208);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(109, 25);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Contraseña:";
+            this.contraseñausuario.AutoSize = true;
+            this.contraseñausuario.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contraseñausuario.Location = new System.Drawing.Point(26, 208);
+            this.contraseñausuario.Name = "contraseñausuario";
+            this.contraseñausuario.Size = new System.Drawing.Size(109, 25);
+            this.contraseñausuario.TabIndex = 12;
+            this.contraseñausuario.Text = "Contraseña:";
             // 
             // textBox5
             // 
@@ -173,34 +177,34 @@
             this.textBox5.Size = new System.Drawing.Size(363, 20);
             this.textBox5.TabIndex = 13;
             // 
-            // comboBox1
+            // clienteaodministrador
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(203, 264);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(155, 21);
-            this.comboBox1.TabIndex = 14;
+            this.clienteaodministrador.FormattingEnabled = true;
+            this.clienteaodministrador.Location = new System.Drawing.Point(203, 264);
+            this.clienteaodministrador.Name = "clienteaodministrador";
+            this.clienteaodministrador.Size = new System.Drawing.Size(155, 21);
+            this.clienteaodministrador.TabIndex = 14;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(811, 336);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.clienteaodministrador);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.contraseñausuario);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.emailusuario);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.apellidousuario);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nombreusuario);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idusuario);
             this.Controls.Add(this.btnModificraperfil);
             this.Controls.Add(this.btnCerarsesion);
-            this.Controls.Add(this.btnIniciarSesion);
-            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnsesion);
+            this.Controls.Add(this.btnregistrar);
             this.Name = "Form1";
             this.Text = ".";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -211,21 +215,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnRegistrar;
-        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Button btnregistrar;
+        private System.Windows.Forms.Button btnsesion;
         private System.Windows.Forms.Button btnCerarsesion;
         private System.Windows.Forms.Button btnModificraperfil;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label idusuario;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label nombreusuario;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label apellidousuario;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label emailusuario;
         private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label contraseñausuario;
         private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox clienteaodministrador;
     }
 }
 
--- HotelBook/Form1.cs
+++ HotelBook/Form1.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using UsuarioLibrary;
 
 namespace HotelBook
 {
-    public partial class Form1: Form
+    public partial class Form1 : Form
     {
+        private Usuario usuario;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,21 +15,127 @@ namespace HotelBook
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text) ||  
+                    string.IsNullOrWhiteSpace(textBox2.Text) ||  
+                    string.IsNullOrWhiteSpace(textBox3.Text) ||  
+                    string.IsNullOrWhiteSpace(textBox4.Text) || 
+                    string.IsNullOrWhiteSpace(textBox5.Text))    
+                {
+                    MessageBox.Show("Todos los campos son obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
 
+                if (!int.TryParse(textBox1.Text, out int idUsuario))
+                {
+                    MessageBox.Show("El ID de usuario debe ser un número válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string nombre = textBox2.Text;
+                string apellido = textBox3.Text;
+                string email = textBox4.Text;
+                string contraseña = textBox5.Text;
+
+                usuario = new Usuario(idUsuario, nombre, apellido, email, contraseña, "Cliente");
+                usuario.Registrar();
+
+                MessageBox.Show("Usuario registrado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al registrar el usuario: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) 
+        {
+            if (usuario != null)
+            {
+                string email = textBox4.Text;
+                string contraseña = textBox5.Text;
+
+                bool sesionIniciada = usuario.IniciarSesion(email, contraseña);
+                MessageBox.Show(sesionIniciada ? "Sesión iniciada con éxito." : "Email o contraseña incorrectos.",
+                                "Iniciar Sesión", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No hay un usuario registrado.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnCerarsesion_Click(object sender, EventArgs e) 
+        {
+            if (usuario != null)
+            {
+                usuario.CerrarSesion();
+                MessageBox.Show("Sesión cerrada con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No hay una sesión activa.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnModificraperfil_Click(object sender, EventArgs e) 
+        {
+            if (usuario != null)
+            {
+                try
+                {
+                    string nombre = textBox2.Text;
+                    string apellido = textBox3.Text;
+                    string email = textBox4.Text;
+                    string contraseña = textBox5.Text;
+
+                    usuario.ModificarPerfil(nombre, apellido, email, contraseña);
+                    MessageBox.Show("Perfil modificado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al modificar el perfil: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No hay un usuario registrado.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void idusuario_Click(object sender, EventArgs e) 
+        {
+            if (usuario != null)
+            {
+                MessageBox.Show($"ID de Usuario: {usuario.IdUsuario}", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No hay un usuario registrado.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void nombreusuario_Click(object sender, EventArgs e) 
+        {
+            if (usuario != null)
+            {
+                MessageBox.Show($"Nombre del Usuario: {usuario.Nombre} {usuario.Apellido}", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("No hay un usuario registrado.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) 
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) 
         {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
+           // usuario = new Usuario(idUsuario, nombre, apellido, email, contraseña, "Cliente");
 
         }
     }
